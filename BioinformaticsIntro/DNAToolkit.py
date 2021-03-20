19a066080419b07406dfe972e8ec2757e87ce4ca
isidoredelpierro <fontadore@gmail.com>
2021-03-20 22:33:29 +0100

    Lesson 3: GC Content Calculator

--- BioinformaticsIntro/DNAToolkit.py
+++ BioinformaticsIntro/DNAToolkit.py
@@ -2,6 +2,7 @@
 #rebelCoder
 import collections
 from structures import *
+#from collections import Counter
 
 #Check the sequence to make sure it is a DNA String
 def validateSeq(dna_seq):
@@ -23,5 +24,22 @@ def transcription(seq):
     return seq.replace("T", "U")
 
 def reverse_complement(seq):
-    """Swapping Adenine with Thymine and Guanine with Cytosine. Reversing newly generated string"""
+    """Swapping Adenine with Thymine and Guanine with Cytosine.
+    Reversing newly generated string"""
     return ''.join([DNA_ReverseComplement[nuc] for nuc in seq])[::-1]
+
+    #Pythonic approach. A little bit faster solution.
+    #mapping * str.maketrans('ATCG', 'TAGC')
+    #return seq.translate(mapping)[::-1]
+
+def gc_content(seq):
+    """GC Content in a DNA/RNA sequence"""
+    return round((seq.count('C') + seq.count('G')) / len(seq) * 100)
+
+def gc_content_subsec(seq, k=20):
+    """GC Content in a DNA/RNA sub-sequence length k, k=20 by default"""
+    res = []
+    for i in range(0, len(seq) - k + 1, k):
+        subseq = seq[i:i + k]
+        res.append(gc_content(subseq))
+    return res
